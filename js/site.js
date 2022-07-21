98424e89f8e58cdfe992303607a42e4e862dd6f8
cg1image <54491781+cg1image@users.noreply.github.com>
2022-07-20 19:23:21 -0700

    fizzbuzz js redo
    redo site.js for better undertanding

--- js/site.js
+++ js/site.js
@@ -1,91 +1,66 @@
-//get values from the user. We need to get the fizz and the buzz value.
+//get values from the user. fizzValue and buzzValue
 function getValues(){
 
-    //get the user values from the DOM
-    let fizzValue = document.getElementById("fizzValue").value;
-    let buzzValue = document.getElementById("buzzValue").value;
-
-    //parse for numbers
+    //get the user values from the page
+    let fizzValue = document.getElementById(fizzValue).value;
+    let buzzValue = document.getElementById(buzzValue).value;
+    //check to make sure they're numbers
     fizzValue = parseInt(fizzValue);
     buzzValue = parseInt(buzzValue);
-
     //check that the numbers are integers
-    if(Number.isInteger(fizzValue) && Number.isInteger(buzzValue)){
-
-        //we call fizzBuzz
-        let fbArray = fizzBuzz(fizzValue, buzzValue);
-
+    if (Numbers.isInteger(fizzValue) && Numbers.isInteger(buzzValue)){
+        
+        //call fizzBuzz
+        let fbArray = fizzBuzz(fizzValue,buzzValue);
         //call displayData and write the values to the screen
         displayData(fbArray);
-        
-    } else{
+
+    } else {
         alert("You must enter integers");
     }
-    
-    
+     
 }
 
+function fizzBuzz(fizzValue,buzzValue){
 
-function fizzBuzz(fizzValue, buzzValue){
-    
-    let returnArray =[];
+    let returnArray = [];
 
+    //iterate the returnArray
     //loop from 1 to 100
-    for (let i = 1; i <= 100; i++){
+    for (let i = 1; i <= 100; i++) {
         
-        //check to see if divisible by Both (3 and 5)
-        //check to see if divisible by fizz value (3)
-        //check to see if divisible by buzz balue (5)
-        if(i % fizzValue == 0 && i % buzzValue == 0){
-            returnArray.push('FizzBuzz');
-        } else if (i % fizzValue == 0) {
-            returnArray.push('Fizz');
-        } else if(i % buzzValue == 0) {
-            returnArray.push('Buzz');
-        } else {
-            returnArray.push(i);
-        }
+    //check the current value in three steps
+    //check to see if divisable by both (3 and 5)
+    if (i % fizzValue ==0 && i % buzzValue ==0){
 
+        //if so push FizzBuzz into an array
+        returnArray.push("FizzBuzz");
+
+    //else check to see if divisable by fizz value (3)
+    } else if (i % fizzValue ==0){
+
+    //if so push Fizz into an array
+        returnArray.push("Fizz");
+
+    //else check to see if divisable by buzz value (5)
+    } else if (i % buzzValue ==0){
+
+    //if so push Buzz into an array
+        returnArray.push("Buzz");
+
+    //else if none then push the number into the array 
+    } else
+    
+        returnArray.push(i);
     }
+
     return returnArray;
 }
 
-//loop over the array and create a tablerow for each item
 function displayData(fbArray){
 
-    //get the table body element from the page
-    let tableBody = document.getElementById("results");
-
-    //get the template row
-    let templateRow = document.getElementById("fbTemplate");
-
-    //clear table first
-    tableBody.innerHTML =  "";
-
-    for (let index = 0; index < fbArray.length; index += 5) {
-        
-        let tableRow = document.importNode(templateRow.content, true);
-
-        //grab use to put into array
-        let rowCols = tableRow.querySelectorAll("td");
-
-        rowCols[0].classList.add(fbArray[index]);
-        rowCols[0].textContent = fbArray[index];
-
-        rowCols[1].classList.add(fbArray[index + 1]);
-        rowCols[1].textContent = fbArray[index + 1];
-        
-        rowCols[2].classList.add(fbArray[index + 2]);
-        rowCols[2].textContent = fbArray[index + 2];
-        
-        rowCols[3].classList.add(fbArray[index + 3]);
-        rowCols[3].textContent = fbArray[index + 3];
-        
-        rowCols[4].classList.add(fbArray[index + 4]);
-        rowCols[4].textContent = fbArray[index + 4];
-
-        tableBody.appendChild(tableRow);
-    }
+    //loop over the array and create a table row for each item
 
     //add all the rows to the table
+
 }
